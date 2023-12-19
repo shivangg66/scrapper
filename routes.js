be788ba2a15c8c8130803141477fb76391de7eb6
Shivang Gupta <shivangg66@gmail.com>
2023-12-19 10:10:45 +0530

    removed unnecessary files

--- routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.api = void 0;
-const express_1 = require("express");
-const scrapping_controller_1 = require("./controllers/scrapping.controller");
-const api = (0, express_1.Router)();
-exports.api = api;
-api.get("/scrapper", scrapping_controller_1.scrappingController);
-//# sourceMappingURL=routes.js.map
